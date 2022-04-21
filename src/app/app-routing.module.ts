44b62b7cddf8733dcf67b4b9c3fe510b0dd00fba
NeoTRAN001 <neo_tran@outlook.com>
2022-04-21 11:18:26 -0500

    feat: Page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,10 @@ const routes: Routes = [
   {
     path: 'category/:id',
     component: CategoryComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { StoreService } from '../../services/store.service';
 import { ProductsService } from '../../services/products.service';
 import { Product } from 'src/app/models/product.model';
 
