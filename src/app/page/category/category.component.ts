f31608879a1cc13cc3b88f8f9e76fd44f5493906
NeoTRAN001 <neo_tran@outlook.com>
2022-04-20 23:50:42 -0500

    feat: get products by categories

--- src/app/page/category/category.component.ts
+++ src/app/page/category/category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categoryId: string | null = null;
+  products: Product[] = [];
+  limit: number = 10;
+  offset: number = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.pipe(
+      switchMap(params => {
+        this.categoryId = params.get('id');
+        if(this.categoryId)
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset);
+        return [];
+      })
+    )
+    .subscribe(data => {
+      this.products = data;
+    });
   }
 
+  loadMore(): void {
+    if(this.categoryId)
+      this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        .subscribe(data => {
+          this.products = this.products.concat(data.filter(product => product.images.length > 0));
+          this.offset += this.limit;
+      });
+  }
 }
