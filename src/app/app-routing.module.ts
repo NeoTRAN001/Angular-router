ff59eff602b3a220eee1f7b2be115d11c30edcbe
NeoTRAN001 <neo_tran@outlook.com>
2022-04-22 20:38:27 -0500

    feat: layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './page/home/home.component';
-import { NotFoundComponent } from './page/not-found/not-found.component';
-import { CategoryComponent } from './page/category/category.component';
-import { MyCartComponent } from './page/my-cart/my-cart.component';
-import { LoginComponent } from './page/login/login.component';
-import { RegisterComponent } from './page/register/register.component';
-import { RecoveryComponent } from './page/recovery/recovery.component';
-import { ProfileComponent } from './page/profile/profile.component';
-import { ProductDetailComponent } from './page/product-detail/product-detail.component';
+import { HomeComponent } from './website/page/home/home.component';
+import { NotFoundComponent } from './website/page/not-found/not-found.component';
+import { CategoryComponent } from './website/page/category/category.component';
+import { LayoutComponent } from './website/components/layout/layout.component';
+import { MyCartComponent } from './website/page/my-cart/my-cart.component';
+import { LoginComponent } from './website/page/login/login.component';
+import { RegisterComponent } from './website/page/register/register.component';
+import { RecoveryComponent } from './website/page/recovery/recovery.component';
+import { ProfileComponent } from './website/page/profile/profile.component';
+import { ProductDetailComponent } from './website/page/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'category/:id',
-    component: CategoryComponent
-  },
-  {
-    path: 'product/:id',
-    component: ProductDetailComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'category/:id',
+        component: CategoryComponent
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent
+      }
+    ]
   },
   {
     path: '**',
