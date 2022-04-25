5a0c35a353f86560cfc1b8be75089086615fa99e
NeoTRAN001 <neo_tran@outlook.com>
2022-04-25 00:13:16 -0500

    feat: global state in auth

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
-import { TokenService } from '@services/token.service';
+import { AuthService } from '@services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +11,22 @@ import { TokenService } from '@services/token.service';
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private tokenService: TokenService,
+    private authService: AuthService,
     private router: Router
   ){}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const token = this.tokenService.getToken();
+      return this.authService.user$.pipe(
+        map(user => {
+          if(!user) {
+            this.router.navigate(['/home'])
+            return false;
+          }
 
-      if(!token) {
-        this.router.navigate(['/home']);
-        return false;
-      }
-
-      return true;
+          return true;
+        })
+      )
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,6 +6,7 @@ import { environment } from './../../environments/environment';
 import { Auth } from './../models/auth.model';
 import { User } from './../models/user.model';
 import { TokenService } from './../services/token.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +14,29 @@ import { TokenService } from './../services/token.service';
 export class AuthService {
 
   private apiUrl = `${environment.API_URL}/api/auth`;
+  private user = new BehaviorSubject<User | null>(null);
 
   constructor(
     private http: HttpClient,
     private tokenService: TokenService
   ) { }
 
+  get user$(): Observable<User | null> {
+    return this.user.asObservable();
+  }
+
+  getProfile() {
+    return this.http.get<User>(`${this.apiUrl}/profile`)
+      .pipe(
+        tap(user => this.user.next(user))
+      );
+  }
+
   login(email: string, password: string) {
     return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password})
     .pipe(
       tap(response => this.tokenService.saveToken(response.access_token))
     );
-  }
-
-  getProfile() {
-    return this.http.get<User>(`${this.apiUrl}/profile`);
   }
 
   loginAndGet(email: string, password: string) {
